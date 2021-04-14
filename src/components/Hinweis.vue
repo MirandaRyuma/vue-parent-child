28d7dba1c602ac4889653944ca008c00d2d7906b
MirandaRyuma <miranda.dominik@gmail.com>
2021-04-14 12:01:45 +0200

    added new stuff

--- src/components/Hinweis.vue
+++ src/components/Hinweis.vue
@@ -1,12 +1,24 @@
 <template>
   <footer class="mt-2">
-    <p>Ich bin nur eine Komponente. Hier gibt es sonst nichts zu sehen.</p>
+    <p>{{ setText }} <br> {{ selectedParent }}</p>
   </footer>         
 </template>
 
 <script>
 export default {
-  name: "Hinweis"  
+  name: "Hinweis",
+  props: {
+      text: String
+  },
+  computed: {
+    setText() {
+        let ausgabe = this.text ? this.text : "Ich bin nur eine Komponente. Hier gibt es sonst nichts zu sehen.";
+        return ausgabe;
+    },
+    selectedParent() {
+        return "ID des aussgewählten Parents: " + this.$store.state.selectedParentId;
+    }
+  } 
 };
 </script>
 
