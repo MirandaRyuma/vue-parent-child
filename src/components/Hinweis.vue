6b58d0e6c0e74b4c51eb88286e2d939b2b08ad6a
MirandaRyuma <miranda.dominik@gmail.com>
2021-04-14 11:47:30 +0200

    created component Hinweis

---
+++ src/components/Hinweis.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="mt-2">
+    <p>Ich bin nur eine Komponente. Hier gibt es sonst nichts zu sehen.</p>
+  </footer>         
+</template>
+
+<script>
+export default {
+  name: "Hinweis"  
+};
+</script>
+
+<style scoped>
+p {
+  background-color: yellow;
+  color: black;
+  padding: 0.2em 0.5em
+}
+</style>
